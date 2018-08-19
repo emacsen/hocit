278aaca6a5691c04aa7d2770d2bbeb37be6b7ec8
Serge Wroclawski <serge@wroclawski.org>
2018-08-19 17:21:57 -0400

    First view (report)

--- hocit/urls.py
+++ hocit/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
+app_name = 'hocit'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('report/', views.report, name='report'),
 ]
---
+++ hocit/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def report(request):
+    return render(request, 'report.html')
+
+def report_update(request):
+    pass
+
+def reports_by_asignee(request):
+    pass
+
+def report_overview(request):
+    pass
+
